c3ed10114a8cef38f83a5f40f02f9380797d0f74
Miguel Soteldo <74800042+STLDOv2@users.noreply.github.com>
2021-01-29 15:18:51 -0600

    Add files via upload

---
+++ Docs/StudentOrderer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Student_Orderer
+{
+    class StudentOrderer
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many students are in your class: ");
+
+            int numOfStudents = Convert.ToInt32(Console.ReadLine());
+
+            string[] students = new string[numOfStudents]; 
+
+            Console.WriteLine("\n\nPlease input the names of your students:");
+
+
+            for (int i = 0; i < students.Length; i++)
+            {
+                students[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("-------------------");
+
+            Array.Sort(students);
+            for (int i = 0; i < students.Length; i++)
+            {
+                 Console.WriteLine(students[i]);
+            }
+           
+
+            Console.ReadLine();
+
+        }
+    }
+}
